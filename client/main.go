d829c4464b704313a9cf53cb1a77294ce6d09d65
Magnus Brattlöf <brattlof.magnus@gmail.com>
2019-05-25 15:03:55 +0200

    hej mange

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"../gchat"
@@ -9,7 +10,14 @@ import (
 )
 
 func main() {
-	var conn *grpc.ClientConn
+	var (
+		conn               *grpc.ClientConn
+		username, password string
+		option             int
+		token              string
+	)
+
+	fmt.Println("Welcome to go-grpc-chat\nWhat would you like to do?")
 
 	conn, err := grpc.Dial(":7777", grpc.WithInsecure())
 	if err != nil {
@@ -19,11 +27,28 @@ func main() {
 	defer conn.Close()
 
 	c := gchat.NewChatServiceClient(conn)
-	resp, err := c.Register(context.Background(), &gchat.UserContent{Username: "mange", Password: "passwd"})
-	if err != nil {
-		log.Fatalf("Error: %v", err)
+
+	for {
+		fmt.Println("1) \t Register")
+		fmt.Println("2) \t Chat")
+		fmt.Println("3) \t Exit")
+		fmt.Scanf("%d", &option)
+
+		switch option {
+		case 1:
+			fmt.Printf("Username: ")
+			fmt.Scanf("%s", &username)
+			fmt.Printf("Password: ")
+			fmt.Scanf("%s", &password)
+
+			token, err := c.Register(context.Background(), &gchat.UserContent{Username: username, Password: password})
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			} else {
+				log.Printf("You have successfully registerd")
+			}
+		case 2:
+
+		}
 	}
-
-	log.Printf("Response from server: %s", resp.Token)
-
 }
--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"crypto/rand"
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -11,7 +12,63 @@ import (
 	"google.golang.org/grpc"
 )
 
+type User struct {
+	username *string
+	password *string
+	token    string
+}
+
+var database []User
+
 type Server struct {
+}
+
+func (s *Server) Register(ctx context.Context, in *gchat.UserContent) (*gchat.RegisterResponse, error) {
+
+	for _, i := range database {
+		if in.Username == *i.username {
+			return nil, errors.New("Username already exists...")
+		}
+	}
+
+	token := tokenGenerator()
+	database = append(database, User{username: &in.Username, password: &in.Password, token: token})
+	fmt.Println(len(database))
+	printUsers()
+	return &gchat.RegisterResponse{Token: token}, nil
+}
+
+func (s *Server) Login(ctx context.Context, in *gchat.UserContent) (*gchat.LoginResponse, error) {
+	fmt.Printf("Your username was: %s and password: %s", in.Username, in.Password)
+	return &gchat.LoginResponse{Token: "hello"}, nil
+}
+
+func (s *Server) Logout(ctx context.Context, in *gchat.UserContent) (*gchat.LogoutResponse, error) {
+	fmt.Printf("Your username was: %s and password: %s", in.Username, in.Password)
+	return &gchat.LogoutResponse{Token: "hello"}, nil
+}
+
+func (s *Server) SendMessage(ctx context.Context, in *gchat.ChatMessage) (*gchat.MessageResponse, error) {
+
+	return &gchat.MessageResponse{Val: true}, nil
+}
+
+func deliver() {
+
+}
+
+// Returns a string with random chars
+func tokenGenerator() string {
+	// Create the slice with
+	b := make([]byte, 4)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+func printUsers() {
+	for _, i := range database {
+		fmt.Println(*i.username)
+	}
 }
 
 func main() {
@@ -27,27 +84,4 @@ func main() {
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Error serving: %v", err)
 	}
-}
-
-func (s *Server) Register(ctx context.Context, in *gchat.UserContent) (*gchat.RegisterResponse, error) {
-	fmt.Printf("Your username was: %s and password: %s", in.Username, in.Password)
-	return &gchat.RegisterResponse{Token: tokenGenerator()}, nil
-}
-
-func (s *Server) Login(ctx context.Context, in *gchat.UserContent) (*gchat.LoginResponse, error) {
-	fmt.Printf("Your username was: %s and password: %s", in.Username, in.Password)
-	return &gchat.LoginResponse{Token: "hello"}, nil
-}
-
-func (s *Server) Logout(ctx context.Context, in *gchat.UserContent) (*gchat.LogoutResponse, error) {
-	fmt.Printf("Your username was: %s and password: %s", in.Username, in.Password)
-	return &gchat.LogoutResponse{Token: "hello"}, nil
-}
-
-// Returns a string with random chars
-func tokenGenerator() string {
-	// Create the slice with
-	b := make([]byte, 4)
-	rand.Read(b)
-	return fmt.Sprintf("%x", b)
 }
